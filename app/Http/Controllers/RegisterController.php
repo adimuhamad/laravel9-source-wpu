297d278c28aeba7718696500381eafd4f74c8c28
adimuhamad <adi.10119253@mahasiswa.unikom.ac.id>
2022-10-03 19:32:05 +0700

    User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,24 @@ class RegisterController extends Controller
       'title' => 'Register',
       'active' => 'register'
     ]);
+  }
+
+  public function store(Request $request)
+  {
+    $validatedData = $request->validate([
+      'name' => 'required|max:100',
+      'username' => 'required|min:5|max:20|unique:users',
+      'email' => 'required|email:dns|unique:users',
+      'password' => 'required|min:10|max:255'
+    ]);
+
+    // $validatedData['password'] = bcrypt($validatedData['password']);
+    $validatedData['password'] = Hash::make($validatedData['password']);
+
+    User::create($validatedData);
+    // $request = session();
+    // $request->flash('success', 'Registration successfully! Please login');
+    // return redirect('/login');
+    return redirect('/login')->with('success', 'Registration successfully! Please login');
   }
 }
